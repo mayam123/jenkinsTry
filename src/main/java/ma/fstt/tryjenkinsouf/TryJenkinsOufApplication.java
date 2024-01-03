f44908c3fa27e1ceaf2cc37e878ea7c37362223f
mayam123 <mariam.slimati@gmail.com>
2024-01-03 22:01:21 +0100

    second commit

--- src/main/java/ma/fstt/tryjenkinsouf/TryJenkinsOufApplication.java
+++ src/main/java/ma/fstt/tryjenkinsouf/TryJenkinsOufApplication.java
@@ -2,16 +2,17 @@ package ma.fstt.tryjenkinsouf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class TryJenkinsOufApplication {
 
-    @GetMapping
-    public String message(){
-        return "welcome to my try";
+    @GetMapping("/")
+    public String displayMessage(){
+        return "Jenkins demo whith Springboot";
     }
     public static void main(String[] args) {
         SpringApplication.run(TryJenkinsOufApplication.class, args);
